91d4316a6aa0665397bb398adefdc349f091a9fc
meng <18813189457@163.com>
2018-07-31 20:41:11 +0800

    //Spring学习

---
+++ spring/src/com/itheima/bean/User.java
@@ -0,0 +1,28 @@
+package com.itheima.bean;
+
+/**
+ * @Author:zhaomeng
+ * @Description:
+ * @Date: Created in 20:29 2018/7/31
+ * @Modified By:
+ */
+public class User {
+    private String name;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
